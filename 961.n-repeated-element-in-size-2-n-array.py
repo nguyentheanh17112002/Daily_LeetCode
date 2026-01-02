96431000174924faee8f93d06debdcfc3559c1f0
nguyentheanh1711 <nguyentheanh17112002@gmail.com>
2026-01-02 10:37:40 +0700

    commit

---
+++ 961.n-repeated-element-in-size-2-n-array.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=961 lang=python3
+#
+# [961] N-Repeated Element in Size 2N Array
+#
+
+# @lc code=start
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
+    
+        
+# @lc code=end
+
